dfea9f4ccabf946abb4b82bb8ec9a3d4d8b851dc
Cláudio Santos <cljos@hotmail.com>
2020-08-02 14:24:12 -0300

    chore: create form categories

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from  '../../../components/PageDefault';
+import { Form, Button, Col } from 'react-bootstrap'
 
 function CadastroCategoria(){
+    const [categorias, setCategorias] = useState(['teste']);
+    
+    const initialValues = {
+        name: '',
+        description: '',
+        color: '#000'
+    }
+
+    const [values, setValues] = useState([initialValues]);
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value,
+        })
+    }
+
+    function handleChange(info){
+        const { getAttribute, value } = info.target;
+        setValue(
+            getAttribute('name'), 
+            value
+        )
+    }
+
     return(
         <>
             <PageDefault>
-                <h1>Cadastro de Categorias</h1>
+                <h1>Cadastro de Categorias: {values.nome}</h1>
 
-                <form action="">
-                    <label htmlFor="">Categoria:
-                    <input type="text"/>
-                    </label>
+                <form onSubmit={function handleSubmit(event){
+                    event.preventDefault();
 
-                    <button>Cadastrar</button>
+                    setValues([
+                        ...categorias,
+                        values.name
+                    ])
+                }}>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="FormGridCategory">
+                            <Form.Label>Categoria:</Form.Label>
+                            <Form.Control name="nome" placeholder="Categoria" value={values.name} onChange={handleChange} />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="FormGridColor">
+                            <Form.Label>Cor:</Form.Label>
+                            <Form.Control name="cor" placeholder="Cor" type="color" value={values.color} onChange={handleChange} />
+                        </Form.Group>
+                    </Form.Row>
+                    <Form.Row>
+                    <Form.Group as={Col} controlId="FormGridDescription">
+                            <Form.Label>Descrição:</Form.Label>
+                            <Form.Control name="descricao" as="textarea" placeholder="Descrição" rows="5" value={values.description} onChange={handleChange} />
+                        </Form.Group>
+                    </Form.Row>
+
+                    <Button variant="primary" type="Submit">Cadastrar</Button>
                 </form>
+
+                <ul>
+                    {categorias.map((categoria, index) => {
+                        return (
+                            <li key={`${categoria}${index}`}>
+                                {categoria.nome}
+                            </li>
+                        )
+                    })}
+                </ul>
 
                 <Link to='/'>Voltar</Link>
             </PageDefault>
